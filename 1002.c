386b0c15fc243c614d92ed6cf30976e95133dab9
LingDu-zero <33028145+LingDu-zero@users.noreply.github.com>
2018-01-09 11:37:37 +0800

    Create 1002.c

---
+++ 1002.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int solution(int m);
+
+int main(void)
+{
+	char a;
+	unsigned long m = 0;
+	while ((a = getchar())!='\n') {
+		 m += a - '0';
+	}
+	solution(m);
+
+	return 0;
+}
+
+int solution(int m) {
+	if (!m) return 0;
+	solution(m / 10);
+	if (m / 10) printf(" ");
+	switch (int n = m % 10) {
+	case 0:printf("ling"); break;
+	case 1:printf("yi"); break;
+	case 2:printf("er"); break;
+	case 3:printf("san"); break;
+	case 4:printf("si"); break;
+	case 5:printf("wu"); break;
+	case 6:printf("liu"); break;
+	case 7:printf("qi"); break;
+	case 8:printf("ba"); break;
+	case 9:printf("jiu"); break;
+	}
+	return 0;
+}
