2437101b9fb01877ff6602b57470e19eb452b135
LingDu-zero <33028145+LingDu-zero@users.noreply.github.com>
2018-01-14 11:44:57 +0800

    Update 1008.c

--- 1008.c
+++ 1008.c
@@ -9,8 +9,7 @@ int main(void)
 	while (m > n) m %= n;
 	int *a;
 	a = (int *)malloc(n*sizeof(int));
-	for (int i = 0; i < n;)
-		scanf("%d",&a[i]);
+	for (int i = 0; i < n;) scanf("%d", &a[i]);
 	reverse(a, 0, n - m - 1);
 	reverse(a, n - m, n - 1);
 	reverse(a, 0, n - 1);
