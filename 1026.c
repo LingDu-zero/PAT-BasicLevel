1719f144e0872c1508fe021df9b0b080db326cb9
LingDu-zero <33028145+LingDu-zero@users.noreply.github.com>
2018-01-23 16:34:04 +0800

    Update 1026.c

--- 1026.c
+++ 1026.c
@@ -4,10 +4,18 @@
 
 int main(void)
 {
-	int c1, c2;
-	scanf("%d%d", &c1, &c2);
-	int t = round(float(c2 - c1) / float(100));
-	printf("%d:%d:%d", t / 3600, (t % 3600) / 60, (t % 3600) % 60);
+	float c1, c2;
+	scanf("%f%f", &c1, &c2);
+	int t = round((c2 - c1) / 100);
+  	int h=t/3600;
+  	if(h<10) printf("0%d:",h);
+  	else printf("%d:",h);
+  	int m=(t%3600)/60;
+  	if(m<10) printf("0%d:",m);
+  	else printf("%d:",m);
+  	int s=(t % 3600) % 60;
+  	if(s<10) printf("0%d",s);
+  	else printf("%d",s);
   
 	return 0;
 }
