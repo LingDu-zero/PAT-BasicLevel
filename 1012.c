aba9d06567354473344a2935b714f95202ca4da8
LingDu-zero <33028145+LingDu-zero@users.noreply.github.com>
2018-01-23 22:39:48 +0800

    Update 1012.c

--- 1012.c
+++ 1012.c
@@ -2,14 +2,22 @@
 
 int main(void)
 {
-	int n, x, A1, A2, A3, A4, A5, flag, k;
-	A1 = A2 = A3 = A4 = A5 = flag = k = 0;
+	int n, x, A1, A2, A3, A5, flag, c, v;
+	float A4, k;
+	A1 = A2 = A3 = A4 = A5 = flag = k = c = v = 0;
 	scanf("%d", &n);
 	while (n--) {
 		scanf("%d", &x);
 		switch (x % 5) {
-		case 0:if (x % 2 == 0) A1 += x; break;
+		case 0:{
+			if (x % 2 == 0) {
+			A1 += x;
+			c = 1;
+			}
+			break;
+		}
 		case 1: {
+			v = 1;
 			if (flag) {
 				A2 -= x;
 				flag = 0;
@@ -25,13 +33,13 @@ int main(void)
 		case 4:if (x > A5) A5 = x; break;
 		}
 	}
-	if (A1) printf("%d", A1);
+	if (c) printf("%d", A1);
 	else printf("N");
-	if (A2) printf(" %d", A2);
+	if (v) printf(" %d", A2);
 	else printf(" N");
 	if (A3) printf(" %d", A3);
 	else printf(" N");
-	if (A4) printf(" %.1f", float(A4) / float(k));
+	if (A4) printf(" %.01f", A4 / k);
 	else printf(" N");
 	if (A5) printf(" %d", A5);
 	else printf(" N");
