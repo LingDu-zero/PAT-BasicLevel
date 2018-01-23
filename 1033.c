57e481d8cec58210a7f1496810ddddc706342d88
LingDu-zero <33028145+LingDu-zero@users.noreply.github.com>
2018-01-23 23:45:13 +0800

    Update 1033.c

--- 1033.c
+++ 1033.c
@@ -1,32 +1,20 @@
 #include <stdio.h>
-#include <string.h>
 #include <ctype.h>
 
 int main(void)
 {
-	char w[100001], t[100001];
-	int flag = 0, c, p = 0;
-	scanf("%s\n%s", w, t);
-	for (int i = 0; i < strlen(w); i++) {
-		if (w[i] == '.' || w[i] == '+' || w[i] == ',' || w[i] == '-') {
-			flag = 1;
-			break;
-		}
+	int a[128] = { 0 },b;
+	while ((b = getchar()) != '\n') {
+		a[b] = 1;
+		a[tolower(b)] = 1;
 	}
-	for (int i = 0; i < strlen(t); i++) {
-		c = 0;
-		if (isupper(t[i]) && flag) continue;
-		for (int j = 0; j < strlen(w); j++) {
-			if (toupper(t[i]) == w[j]) {
-				c = 1;
-				break;
-			}
-		}
-		if (c) continue;
-		p = 1;
-		printf("%c", t[i]);
+	if (a['+'])
+		for (int i = 'A'; i <= 'Z'; i++)
+			a[i] = 1;
+	while ((b = getchar()) != '\n') {
+		if (a[b]) continue;
+		printf("%c", b);
 	}
-	if (!p) printf("\n");
 
 	return 0;
 }
