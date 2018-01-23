2f76409039cb891f33b544816021e9391cacebec
LingDu-zero <33028145+LingDu-zero@users.noreply.github.com>
2018-01-23 22:52:49 +0800

    Update 1009.c

--- 1009.c
+++ 1009.c
@@ -4,13 +4,13 @@
 int main(void)
 {
 	char s[81];
-	gets_s(s, 80);
+	gets(s);
 	int x = strlen(s) - 1;
 	for (int i = x; i > -1; i--) {
 		if (s[i] != ' ') continue;
 		for (int j = i + 1; j <= x; j++) printf("%c", s[j]);
 		printf(" ");
-		x = --i;
+		x = i - 1;
 	}
 	for (int i = 0; i <= x; i++) printf("%c", s[i]);
 
